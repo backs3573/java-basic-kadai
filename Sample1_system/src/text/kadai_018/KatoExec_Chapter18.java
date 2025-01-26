dc759060cdbb9f5b1a4b2874e20636b8fc98be8e
User <User@R734>
2025-01-26 15:00:22 +0900

    samurai継承の課題の提出

---
+++ Sample1_system/src/text/kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,18 @@
+package text.kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		//加藤太郎のインスタンスを作る
+		KatoTaro_Chapter18 Taro = new KatoTaro_Chapter18();
+		Taro.execIntroduce();
+		//加藤一郎のインスタンスを作る
+		KatoIchiro_Chapter18 Ichiro = new KatoIchiro_Chapter18();
+		Ichiro.execIntroduce();
+		//加藤花子のインスタンスを作る
+		KatoHanako_Chapter18 Hanako = new KatoHanako_Chapter18();
+		Hanako.execIntroduce();
+
+	}
+
+}
